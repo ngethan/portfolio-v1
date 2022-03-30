70e3a3de32437af70e9e31cc2ebc91fa045a8667
intuitiveen <intuitiveen@gmail.com>
2022-03-29 21:16:28 -0400

    concision and animation cleanup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,21 +38,18 @@ const Home = (data) => {
                 <motion.p className="text-[#e98074] text-xl font-code" variants={item}>
                     Hi, my name is
                 </motion.p>
-                <div>
-                    <motion.div
-                        className="name flex text-[0px] whitespace-nowrap text-[#c7d7ed] text-7xl sm:text-8xl font-bold py-4"
-                        variants={item}
-                    >
-                        <motion.h1 className="duration-300 inline-block align-top hover:text-[#E85A4F]">E</motion.h1>
-                        <motion.h1 className="duration-300 inline-block align-top hover:text-[#E85A4F]">t</motion.h1>
-                        <motion.h1 className="duration-300 inline-block align-top hover:text-[#E85A4F]">h</motion.h1>
-                        <motion.h1 className="duration-300 inline-block align-top hover:text-[#E85A4F]">a</motion.h1>
-                        <motion.h1 className="duration-300 inline-block align-top hover:text-[#E85A4F]">n</motion.h1>
-                        &nbsp;
-                        <motion.h1 className="duration-300 inline-block align-top hover:text-[#E85A4F]">N</motion.h1>
-                        <motion.h1 className="duration-300 inline-block align-top hover:text-[#E85A4F]">g</motion.h1>
-                    </motion.div>
-                </div>
+                <motion.div
+                    className="name flex text-[0px] whitespace-nowrap text-[#c7d7ed] text-7xl sm:text-8xl font-bold py-4"
+                    variants={item}
+                >
+                    {["E", "t", "h", "a", "n", "\xa0", "N", "g"].map((letter) => {
+                        return (
+                            <motion.h1 className="duration-300 inline-block align-top hover:text-[#E85A4F]">
+                                {letter}
+                            </motion.h1>
+                        );
+                    })}
+                </motion.div>
                 <motion.h2 className="text-[#8792a1] text-5xl sm:text-6xl font-bold" variants={item}>
                     Full stack developer
                 </motion.h2>
@@ -78,14 +75,11 @@ const Home = (data) => {
                     .
                 </motion.p>
 
-                <div>
-                    <motion.button
-                        className="text-[#E85A4F] border-[#E85A4F] font-code text-lg border-2 rounded px-6 py-3 my-2 flex items-center duration-300 test hover:bg-[#E85A4F]/[.2]"
-                        variants={item}
-                    >
+                <motion.div variants={item}>
+                    <button className="text-[#E85A4F] border-[#E85A4F] font-code text-lg border-2 rounded px-6 py-3 my-2 flex items-center duration-300 test hover:bg-[#E85A4F]/[.2]">
                         View work
-                    </motion.button>
-                </div>
+                    </button>
+                </motion.div>
             </motion.div>
         </div>
     );
