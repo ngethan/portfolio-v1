45335829ecccf1f8c918aafa67401e6b3a2eec04
intuitiveen <intuitiveen@gmail.com>
2022-05-01 10:55:46 -0400

    removed unused var

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
